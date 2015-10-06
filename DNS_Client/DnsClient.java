27c4f88b9d10c0757743a1dc6058bb666b26393c
liyangRich <liyang.cheung@gmail.com>
2015-10-05 23:38:36 -0400

    Testing 2

--- DNS_Client/DnsClient.java
+++ DNS_Client/DnsClient.java
@@ -8,22 +8,11 @@ Wed08
 DnsClient.java
 */
 
-import java.io.*
-import java.net.*
+import java.io.*;
+import java.net.*;
 
 public class DnsClient {
-
-	/*
-+---------------------+
-Header 			
-+---------------------+
-| Question 				| the question for the name server
-+---------------------+
-| Answer 				| answers to the question
-+---------------------+
-| Authority 			| RRs pointing toward an authority (not used in this lab)
-+---------------------+
-| Additional 			| RRs holding additional information
-+---------------------+
-	*/
+	//
+	//
+	//TO DO:
 }
