fdea657a57ad09a2c4356d54a4bfd5a2bf42acac
liyangRich <liyang.cheung@gmail.com>
2015-10-05 23:26:33 -0400

    Testing

--- DNS_Client/DnsClient.java
+++ DNS_Client/DnsClient.java
@@ -1,1 +1,29 @@
+/*
+ECSE 489 Experiment 2
+Networking Programming and DNS
 
+Ryan Martis		260465757
+Richard Cheung	260494981
+Wed08
+DnsClient.java
+*/
+
+import java.io.*
+import java.net.*
+
+public class DnsClient {
+
+	/*
++---------------------+
+Header 			
++---------------------+
+| Question 				| the question for the name server
++---------------------+
+| Answer 				| answers to the question
++---------------------+
+| Authority 			| RRs pointing toward an authority (not used in this lab)
++---------------------+
+| Additional 			| RRs holding additional information
++---------------------+
+	*/
+}
