e2953df167e04a58f6a5c613c830f3e167bd7a89
richardCheung1 <li.cheung@mail.mcgill.ca>
2015-10-10 17:43:05 -0400

    Added Ip validator and command line validator

--- DNS_Client/DnsClient.java
+++ DNS_Client/DnsClient.java
@@ -10,6 +10,8 @@ DnsClient.java
 
 import java.io.*;
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DnsClient {
 	//
@@ -19,43 +21,80 @@ public class DnsClient {
 	private int PORT_DEFAULT = 53; 
 	private String server, name; 
 
+	private static final String IPADDRESS_PATTERN = 
+		"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
 	public static void main (String[] args) {
-		int argsArraySize = args.length; 
+		int argsArraySize = args.length;
+		if (argsArraySize < 2) {
+			syntaxError(); 
+			return;
+		}
+		if (!args[argsArraySize-1].contains("@")) {
+			syntaxError(); 
+			return;
+		}
+		else {
+			String substring = args[argsArraySize-1].substring(1); 
+			if (!ipAddressValidator(substring)){
+				System.out.println("Syntax error on IP adress"); 
+				return;
+			}
+		}
+		boolean flag = false; 
 		for (int i = 0 ; i< argsArraySize; i++ ) {
 			 switch (args[i]) {
+			 	//TO DO: timeout,max-retries,port,mx or ns flag, server, name
+
 			 	case "-t":
-			 		System.out.println("Timeout set to"+args[i++]); 
+			 		flag= isNumeric(args[i+1]);
 			 		break;	
 			 	case "-r":
-			 		System.out.println("Max retries set to"+args[i++]);
+			 		flag= isNumeric(args[i+1]);		 	
 			 		break;
 			 	case "-p":
-			 		System.out.println("Port set to"+args[i++]);
+			 		flag= isNumeric(args[i+1]);			 	
 			 		break;
 			 	case "-mx":
-			 		System.out.println("Mail server");
 			 		break;
 			 	case "-ns":
-			 		System.out.println("Name Server");
 			 		break; 			
+			 }
+			 if (!flag) {
+			 	syntaxError(); 
+			 	return; 
 			 }
 		}
 
 	}
 
-	private static void checkSyntax ( String[] args) {
-		int argsArraySize = args.length; 
-		for (int i = 0 ; i< argsArraySize; i++ ) {
-			if
+	private static boolean ipAddressValidator (String ip) {
+		
+		Pattern pattern = Pattern.compile(IPADDRESS_PATTERN); 
+		Matcher matcher = pattern.matcher(ip);
+		return matcher.matches(); 
+	}
 
-		}
-
+	private static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
 	}
 
 	private static void syntaxError() {
-		System.out.println ("ERROR: Syntax should be DnsClient [-t timeout] [-r max-retries] [-p port] [-mx/-ns] @server name");
+		System.out.println("ERROR: Syntax should be:");
+		System.out.println("DnsClient [-t timeout] [-r max-retries] [-p port] [-mx/-ns] @server name");
 	}
-	//TO DO: timeout,max-retries,port,mx or ns flag, server, name
 
 
 }
